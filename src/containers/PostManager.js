9ae1037f99bcf3265d324d41a9d91c753b366878
AlexHunton2 <alexhunton02@gmail.com>
2022-01-22 16:45:03 -0500

    Upvote system

--- src/containers/PostManager.js
+++ src/containers/PostManager.js
@@ -1,17 +1,34 @@
 import { Row, Col, Container } from "react-bootstrap";
 import Post from "../components/Post";
+import PostAlg from "../utils/PostAlg";
+
+function getPostsWithScore(posts) {
+    var postsWithScore = {};
+    var score = 0;
+    for (var i = 0; i < posts.length; i++) {
+        score = PostAlg(posts[i][1].votes, 0, posts[i][1].date, posts[i][1].date);
+        postsWithScore[score] = posts[i];
+    }
+    return postsWithScore;
+}
 
 function PostManager({posts}) {
+    const numberSort = function (a,b) {
+        return a - b;
+    };
+    const postsWithScore = getPostsWithScore(posts);
+    const sortedScores = Object.keys(postsWithScore).sort(numberSort).reverse();
+    
     return (
         <div>
             <br />
             <Container flud="md">
             {
-                posts.map((post, i) => {
+                sortedScores.map((score, i) => {
                     return (
                     <div key={i}>
                         <Row className="g-4">
-                            <Post postId={post[0]} post={post[1]}/>
+                            <Post postId={postsWithScore[score][0]} post={postsWithScore[score][1]}/>
                         </Row>
                     </div> )
                 })
--- src/utils/PostAlg.js
+++ src/utils/PostAlg.js
@@ -1,7 +1,17 @@
 function PostAlg(upvotes, views, time_creation, time_update) {
-    var score = (upotes + (.002 * views) + 0.75);
-    var decay = (1 + Math.pow(time_creation, 1.8) - Math.pow(time_creation - time_update, 1.2));
-    return score / decay;
+    var score = (upvotes + (.002 * views) + 0.75);
+
+    var time_of_creation = (new Date(time_creation)).getTime()
+    var time_of_update = ((new Date(time_update)).getTime())
+    
+    var time_since_creation = (new Date()).getTime() - time_of_creation;
+    var time_since_update = (new Date()).getTime() - time_of_update;
+
+    var hours_since_creation = (time_since_creation / (1000 * 60 * 60)).toFixed(1);
+    var hours_since_update = (time_since_update / (1000 * 60 * 60)).toFixed(1);
+
+    var decay = 1 + Math.pow(hours_since_creation, 1.8) - Math.pow(hours_since_creation - hours_since_update, 1.2);
+    return (score / decay);
 }
 
 export default PostAlg;
