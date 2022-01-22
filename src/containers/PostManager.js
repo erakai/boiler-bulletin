a41df245bdf075f04099b0cd1c2b40cddbc33a76
erakai <kaitinkess@gmail.com>
2022-01-22 18:30:17 -0500

    temporary fix to algo

--- src/containers/PostManager.js
+++ src/containers/PostManager.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import { Row, Col, Container } from "react-bootstrap";
 import Post from "../components/Post";
 import PostAlg from "../utils/PostAlg";
@@ -6,29 +7,50 @@ function getPostsWithScore(posts) {
     var postsWithScore = {};
     var score = 0;
     for (var i = 0; i < posts.length; i++) {
-        score = PostAlg(posts[i][1].votes, 0, posts[i][1].date, posts[i][1].date);
+        score = PostAlg(posts[i][1].votes, 0, posts[i][1].date, posts[i][1].date) + Math.floor(Math.random() * 100);
         postsWithScore[score] = posts[i];
     }
     return postsWithScore;
 }
 
 function PostManager({posts}) {
+    const [sortedPosts, setSortedPosts] = useState({
+        postDict: {},
+        sortedScores: []
+    })
+
     const numberSort = function (a,b) {
-        return a - b;
+        return Number(a) - Number(b);
     };
-    const postsWithScore = getPostsWithScore(posts);
-    const sortedScores = Object.keys(postsWithScore).sort(numberSort).reverse();
+
+    useEffect(() => {
+        console.log("UPDATED DICTIONARY AND SCORES")
+        var scores = getPostsWithScore(posts)
+        console.log(scores)
+        setSortedPosts({
+            postDict: scores,
+            sortedScores: Object.keys(scores).sort(numberSort).reverse() 
+        })
+
+        return () => {
+            setSortedPosts({
+                postDict: {},
+                sortedScores: []
+            })
+        }
+    }, [posts])
+
     
     return (
         <div>
             <br />
             <Container flud="md">
             {
-                sortedScores.map((score, i) => {
+                sortedPosts.sortedScores.map((score, i) => {
                     return (
                     <div key={i}>
                         <Row className="g-4">
-                            <Post postId={postsWithScore[score][0]} post={postsWithScore[score][1]}/>
+                            <Post postId={sortedPosts.postDict[score][0]} post={sortedPosts.postDict[score][1]}/>
                         </Row>
                     </div> )
                 })
